472183dd2339c5461b0771422dcb33892f8662c9
Viktor Gergely <viktor.gergely@erlang-solutions.com>
2021-03-09 18:22:34 +0100

    [add] initial elixir app, doc, .gitignore update

---
+++ lib/loadex.ex
@@ -0,0 +1,18 @@
+defmodule Loadex do
+  @moduledoc """
+  Documentation for `Loadex`.
+  """
+
+  @doc """
+  Hello world.
+
+  ## Examples
+
+      iex> Loadex.hello()
+      :world
+
+  """
+  def hello do
+    :world
+  end
+end
